aa47fab30349c741d8541ff710aafb316f33ad85
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-02-17 18:13:59 -0700

    setup simple components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import Header from './components/header';
+import SideNav from './components/sideNav';
+
+import MyData from './views/myData';
 
 export default class App extends Component {
   render() {
+    var styles = {
+      viewport: {
+        width: '80vw',
+        minHeight: 'calc( 100vh - 60px )',
+        overflowY: 'auto',
+        float: 'left',
+        boxSizing: 'border-box',
+        backgroundColor: '#C2BDBD',
+        padding: '5px'
+      }
+    };
+
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <Header />
+        <SideNav />
+        <div style={styles.viewport}>
+          <MyData />
+        </div>
+      </div>
     );
   }
 }
